1120e49b4e621d4e074b0b1d7645ef09a1cba544
xx xx xx <xx@xx.xx>
2026-03-24 00:31:56 +0800

    cleanup

--- app.py
+++ app.py
@@ -9,12 +9,7 @@ pages = [
         "pages/20260201_annualized_return_volatility.py",
         title="2026-02-01: Annualized Return & Volatility",
         icon="📊",
-    ),
-    st.Page(
-        "pages/20260322_black_scholes.py",
-        title="2026-03-22: Black-Scholes Model",
-        icon="📈",
-    ),
+    )
 ]
 
 nav = st.navigation(pages)
